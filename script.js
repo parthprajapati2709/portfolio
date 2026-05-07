917ea740a86412a6486dea98025fd54e696ec8eb
parthprajapati2709 <parthprajapati18137@gmail.com>
2026-05-07 17:57:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,18 @@ var typed = new Typed(".typing", {
     loop:true
 
 });
+// =========================
+// MOBILE MENU
+// =========================
+
+const menuToggle =
+document.getElementById("menu-toggle");
+
+const navLinks =
+document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", ()=>{
+
+    navLinks.classList.toggle("active");
+
+});
